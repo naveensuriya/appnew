c35a9f645842e2c54932cceb1e14e5aa921f4cc1
naveen <naveensuriya1999@gmail.com>
2022-09-23 12:44:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route} from "react-router-dom"
+import Login from "./component/Login";
+import Signup from "./component/Signup"
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Cart from "./component/Cart"
+import Deliverystatus from "./component/Deliverystatus";
+import Admindashboard from "./component/Admindashboard";
+import Additems from "./component/Additems"
+import Viewitems from "./component/Viewitems"
+import Viewprofile from "./component/Viewprofile"
+import Payment from "./component/Payment"
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div>
+    <div className="main" >
+<Header/>
+</div>
+<Routes>
+        <Route path="" element={<Home/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="viewprofile" element={<Viewprofile/>}/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="admindashboard" element={<Admindashboard/>}>
+          <Route path="additems" element={<Additems/>}/>
+          <Route path="viewitems" element={<Viewitems/>}/>
+          </Route>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="payment" element={<Payment/>}/>
+        <Route path="delivery" element={<Deliverystatus/>}/>
+      </Routes>
     </div>
   );
 }
