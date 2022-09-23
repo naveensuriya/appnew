9dac6f1bf79231d4b21cb2e2472d7240c978a779
naveen <naveensuriya1999@gmail.com>
2022-09-23 11:49:50 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const exp=require("express")
+const app=exp()
+const cors = require('cors')
+
+const userapp=require("./Apis/userapi")
+const adminapp=require("./Apis/adminapi")
+require("dotenv").config()
+
+const mongoose=require("mongoose")
+//const path=require("path")
+
+// connect express server with react
+//app.use(exp.static(path.join(__dirname,"./build")))
+app.use(cors())
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=>console.log("database is connected successfully"))
+.catch((err)=>console.log("err is",err))
+
+app.use('/user',userapp)
+app.use('/admin',adminapp)
+
+const port=process.env.PORT
+app.listen(port,()=>console.log(`server is running on port ${port}`))
